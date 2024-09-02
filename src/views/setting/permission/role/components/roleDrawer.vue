f84964b4a0d8584c9e43d87ba9879fccf74f84eb
aven <17516019587@163.com>
2024-09-02 16:53:25 +0800

    feat(view): 🚀 菜单权限分配按钮级别父子不关联
    角色管理-分配菜单树结构按钮一级不关联

--- src/views/setting/permission/role/components/roleDrawer.vue
+++ src/views/setting/permission/role/components/roleDrawer.vue
@@ -5,23 +5,13 @@
     </template>
     <template #default>
       <div class="form-layout-wrapper">
-        <el-tree
-          ref="treeRef"
-          style="max-width: 600px"
-          :data="treeData"
-          show-checkbox
-          default-expand-all
-          node-key="id"
-          :props="defaultProps"
-          check-strictly
-        />
+        <el-tree ref="treeRef" style="max-width: 600px" :data="treeData" show-checkbox default-expand-all node-key="id"
+          :props="defaultProps" check-strictly @check="hanleCheck" @check-change="checkChange" />
       </div>
     </template>
     <template #footer>
       <div style="flex: auto">
-        <el-button type="primary" @click="drawerConfirm" :loading="loading"
-          >保存</el-button
-        >
+        <el-button type="primary" @click="drawerConfirm" :loading="loading">保存</el-button>
         <el-button @click="drawerCancel">取消</el-button>
       </div>
     </template>
@@ -29,11 +19,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
 import { ElMessage } from "element-plus";
 import { reqMenuList } from "@/api/menu";
 import { reqGetPermission } from "@/api/role";
 import { formatTime } from "@/utils/tool";
+import { TreeNode, TreeNodeData } from "element-plus/es/components/tree-v2/src/types";
 
 const defaultProps = {
   children: "children",
@@ -100,6 +91,17 @@ const getSelectPerssion = async () => {
   if (code === 200) {
     const selectTreeIds = data.list;
     treeRef.value!.setCheckedKeys(selectTreeIds);
+    // 处理回显选中Tree树结构逻辑
+    await nextTick();
+    selectTreeIds.forEach((id) => {
+      const nodeData = treeRef.value!.getNode(id); // 根据权限id，获取每个选中的树节点
+      console.log("nodeData", nodeData);
+      console.log("nodeData.parent?.id", nodeData.parent?.id);
+      // 反显，上面setCheckedKeys后，如果父级没有选中，则把半选状态设置true
+      if (nodeData?.parent?.id != 0 && !nodeData?.parent?.checked) {
+        nodeData.parent.indeterminate = true;
+      }
+    });
   }
 };
 type AcceptParams = {
@@ -116,6 +118,63 @@ const acceptParams = (params: AcceptParams) => {
 defineExpose({
   acceptParams,
 });
+
+// 父子不关联，实现非最后一级，选中关联子数据功能
+const hanleCheck = (data: TreeNodeData, node: TreeNode) => {
+  console.log(data, node);
+  // 获取当前节点是否被选中
+  const isChecked = treeRef.value!.getNode(data).checked;
+  // 如果当前节点被选中，则遍历下级子节点并选中，如果当前节点取消选中，则遍历下级节点并取消
+  if (isChecked) {
+    // 判断该节点是否有下级节点，如果有那么遍历设置下级节点为选中
+    data.children &&
+      data.children.length > 0 &&
+      setChildreChecked(data.children, true);
+  } else {
+    // 如果节点取消选中，则取消该节点下的子节点选中
+    data.children &&
+      data.children.length > 0 &&
+      setChildreChecked(data.children, false);
+  }
+  function setChildreChecked(node: any, isChecked: boolean) {
+    node.forEach((item: any) => {
+      item.children &&
+        item.children.length > 0 &&
+        setChildreChecked(item.children, isChecked);
+      // 修改勾选状态
+      treeRef.value!.setChecked(item.id, isChecked, true);
+    });
+  }
+};
+const checkChange = (data: TreeNodeData, checked: boolean, indeterminate: boolean) => {
+  console.log(data, checked, indeterminate);
+  // 选中全部子节点，父节点也默认选中，但是子节点再次取消勾选或者全部子节点取消勾选也不会影响父节点勾选状态
+  let checkNode = treeRef.value!.getNode(data); //获取当前节点
+
+  // 勾选部分子节点，父节点变为半选状态
+  if (
+    checkNode.parent &&
+    checkNode.parent.childNodes.some((ele: TreeNodeData) => ele.checked)
+  ) {
+    checkNode.parent.indeterminate = true;
+  }
+  // 勾选全部子节点，父节点变为全选状态
+  if (
+    checkNode.parent &&
+    checkNode.parent.childNodes.every((ele: TreeNodeData) => ele.checked)
+  ) {
+    checkNode.parent.checked = true;
+    checkNode.parent.indeterminate = false;
+  }
+  // 如果取消所有第二节点的勾选状态，则第一层父节点也取消勾选
+  if (
+    checkNode.level == 2 &&
+    checkNode.parent.childNodes.every((ele: TreeNodeData) => !ele.checked)
+  ) {
+    checkNode.parent.checked = false;
+    checkNode.parent.indeterminate = false;
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
